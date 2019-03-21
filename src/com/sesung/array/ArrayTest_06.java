aa229411a82977604f6678f466c9ca48e6e22435
Cameobc <dkqtk@naver.com>
2019-03-21 19:57:19 +0900

    Update ArrayTest_06.java

--- src/com/sesung/array/ArrayTest_06.java
+++ src/com/sesung/array/ArrayTest_06.java
@@ -20,7 +20,7 @@ public class ArrayTest_06 {
 		//그 학생의 정보만 출력
 		//4.프로그램 종료
 		Scanner sc = new Scanner(System.in);
-		int [] numbers = null; // 학생번호저장
+		int [] numbers = null; // 학생번호저장  선언부
 		String [] names = null; //학생이름저장
 		int [] kors = null; // 국어점수
 		int [] math = null;//수학점수
@@ -31,30 +31,61 @@ public class ArrayTest_06 {
 			System.out.println("2.전체정보 조회");
 			System.out.println("3.학생정보 검색");
 			System.out.println("4.프로그램 종료");
-			int select = sc.nextInt();			
+			int select = sc.nextInt();
+			
+			
 			if(select==1) {
 				System.out.println("학생 수를 입력하세요.");
 				int count = sc.nextInt();
 				
+				names= new String [count];   //배열 생성 길이가 count인 배열을 생성한 것이다.  생성부
+				numbers = new int[count];
+				kors = new int[count];
+				math = new int[count];
+				eng = new int[count];
 				
-				for(int i=0;i<count.length;i+!!) {
-					System.out.println("학생 번호를 입력하세요.");
-					numbers = new int[count];
+				
+				for(int i=0;i<names.length;i++) {
+					System.out.println(i+1+"학생 번호를 입력하세요.");
+					numbers[i] = sc.nextInt();
+					
 					System.out.println("학생의 이름을 입력하세요.");
-					names = new String[count];
+					names[i]=sc.next(); 
+			
 					System.out.println("학생의 국어 점수를 입력하세요.");
-					kors = new int[count];
+					kors[i] = sc.nextInt();
+			
 					System.out.println("학생의 영어 점수를 입력하세요.");
-					eng = new int[count];
-					System.out.println("학생의 수학 점수를 입력하세요.");
-					math = new int[count];
-				}//for end
+					eng[i]=sc.nextInt();
+		
+					System.out.println("학생의 수학 점수를 입력하세요.");					
+					eng[i]=sc.nextInt();
+			}//for end
 
 
 
-			}//if select 1
+		}if(select==2) {
+			for(int i=0;i<names.length;i++) {
+			System.out.println("학생 번호 : "+numbers[i]+" 학생 이름 : "+names[i]+" 국어 점수 : "+kors[i]+" 영어 점수 : "+eng[i]+" 수학 점수 :"+math[i]);
+			}
+			
+		}if(select==3) {
+			for(int i=0;i<names.length;i++) {
+				System.out.println("검색할 학생의 번호를 입력하세요.");
+				for(int j=0;j<names.length;j++) {
+					numbers[j] = sc.nextInt();
+				}
+				System.out.println("학생 번호 : "+numbers[i]+" 학생 이름 : "+names[i]+" 국어 점수 : "+kors[i]+" 영어 점수 : "+eng[i]+" 수학 점수 :"+math[i]");
+				break;
+				
+			}
+			
+		}if(select==4) {
+			System.out.println("프로그램을 종료합니다.");
+			break;
+		}
 
-		}//while end
-	}
+	}//while end
+}
 
 }
